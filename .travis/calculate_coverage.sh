d0bf1b018aecef4d32d76d85c81483893e9da6c9
Julian Schmid <info@julianschmid.name>
2018-09-16 13:24:17 +0200

    Added travis configuration.

---
+++ .travis/calculate_coverage.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    # Don't calculate the coverage on os x
+    echo "Skipping code coverage calculation on OS X."
+else
+    # On Linux build instrumented for coverage and run the tests
+    wget https://github.com/SimonKagstrom/kcov/archive/v36.tar.gz &&
+    tar xzf v36.tar.gz &&
+    cd kcov-36 &&
+    mkdir build &&
+    cd build &&
+    cmake .. &&
+    make &&
+    make install DESTDIR=../../kcov-build &&
+    cd ../.. &&
+    rm -rf kcov-36 &&
+    for file in target/debug/dlt_parse-*[^\.d]; do mkdir -p "target/cov/$(basename $file)"; ./kcov-build/usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"; done &&
+    bash <(curl -s https://codecov.io/bash) &&
+    echo "Uploaded code coverage"
+fi
