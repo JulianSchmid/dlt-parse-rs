6745ddcc096bd2613f7dc2f9f93bec89c7e6cbd4
Julian Schmid <info@julianschmid.name>
2022-08-02 07:51:24 +0200

    Added storage header implementation

--- src/lib.rs
+++ src/lib.rs
@@ -124,8 +124,15 @@ pub enum ReadError {
         required_length: usize,
         length: usize,
     },
+    StorageHeaderStartPattern(error::StorageHeaderStartPatternError),
     ///Standard io error.
     IoError(io::Error),
+}
+
+impl From<error::StorageHeaderStartPatternError> for ReadError {
+    fn from(err: error::StorageHeaderStartPatternError) -> ReadError {
+        ReadError::StorageHeaderStartPattern(err)
+    }
 }
 
 impl From<io::Error> for ReadError {
@@ -136,8 +143,10 @@ impl From<io::Error> for ReadError {
 
 impl std::error::Error for ReadError {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        use ReadError::*;
         match self {
-            ReadError::IoError(ref err) => Some(err),
+            StorageHeaderStartPattern(ref err) => Some(err),
+            IoError(ref err) => Some(err),
             _ => None,
         }
     }
@@ -157,6 +166,7 @@ impl fmt::Display for ReadError {
             } => {
                 write!(f, "ReadError: The length of {} in the dlt header is smaller then minimum required size of {} bytes.", length, required_length)
             }
+            StorageHeaderStartPattern(err) => err.fmt(f),
             IoError(err) => err.fmt(f),
         }
     }
@@ -1087,6 +1097,83 @@ impl<'a> Iterator for SliceIterator<'a> {
         } else {
             None
         }
+    }
+}
+
+/// Header present before a `DltHeader` if a DLT packet is
+/// stored in .dlt file or database.
+pub struct StorageHeader {
+    pub timestamp_seconds: u32,
+    pub timestamp_microseconds: u32,
+    pub ecu_id: [u8;4],
+}
+
+impl StorageHeader {
+
+    /// Pattern/Magic Number that must be present at the start of a storage header.
+    pub const PATTERN_AT_START: [u8;4] = [0x44, 0x4C, 0x54, 0x01];
+
+    /// Returns the serialized from of the header.
+    pub fn to_bytes(&self) -> [u8;16] {
+        let ts = self.timestamp_seconds.to_be_bytes();
+        let tms = self.timestamp_microseconds.to_be_bytes();
+        [
+            StorageHeader::PATTERN_AT_START[0],
+            StorageHeader::PATTERN_AT_START[1],
+            StorageHeader::PATTERN_AT_START[2],
+            StorageHeader::PATTERN_AT_START[3],
+            ts[0],
+            ts[1],
+            ts[2],
+            ts[3],
+            tms[0],
+            tms[1],
+            tms[2],
+            tms[3],
+            self.ecu_id[0],
+            self.ecu_id[1],
+            self.ecu_id[2],
+            self.ecu_id[3],
+        ]
+    }
+
+    /// Tries to decode a storage header.
+    pub fn from_bytes(bytes: [u8;16]) -> Result<StorageHeader, error::StorageHeaderStartPatternError> {
+        let start_pattern = [
+            bytes[0], bytes[1], bytes[2], bytes[3],
+        ];
+        if start_pattern != StorageHeader::PATTERN_AT_START {
+            Err(
+                error::StorageHeaderStartPatternError{
+                    actual_pattern: start_pattern,
+                }
+            )
+        } else {
+            Ok(StorageHeader{
+                timestamp_seconds: u32::from_be_bytes([
+                    bytes[4], bytes[5], bytes[6], bytes[7]
+                ]),
+                timestamp_microseconds: u32::from_be_bytes([
+                    bytes[8], bytes[9], bytes[10], bytes[11]
+                ]),
+                ecu_id: [
+                    bytes[12], bytes[13], bytes[14], bytes[15]
+                ],
+            })
+        }
+    }
+
+    ///Deserialize a DltHeader & TpHeader from the given reader.
+    pub fn read<T: io::Read + Sized>(reader: &mut T) -> Result<StorageHeader, ReadError> {
+        let mut bytes: [u8;16] = [0;16];
+        reader.read_exact(&mut bytes)?;
+        Ok(StorageHeader::from_bytes(bytes)?)
+    }
+
+    ///Serializes the header to the given writer.
+    pub fn write<T: io::Write + Sized>(&self, writer: &mut T) -> Result<(), WriteError> {
+        writer.write_all(&self.to_bytes())?;
+        Ok(())
     }
 }
 
