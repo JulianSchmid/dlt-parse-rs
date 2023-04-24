f2e9cdd2d2a44065207db448e9ed30d6b417488d
Alexander Fink <alexander.af.fink@bmw.de>
2023-04-24 16:26:33 +0200

    Added support for strings & signed integers

--- src/verbose/verbose_value.rs
+++ src/verbose/verbose_value.rs
@@ -1,7 +1,15 @@
+extern crate alloc;
+
+use alloc::borrow::ToOwned;
+use alloc::string::String;
+
+use crate::error::VerboseDecodeError;
+
 use super::*;
 
 use core::slice;
 use core::str;
+use std::println;
 
 #[derive(Debug, PartialEq)]
 pub enum VerboseValue<'a> {
@@ -37,6 +45,7 @@ impl<'a> VerboseValue<'a> {
 
         // check that enough data for the type info is present
         if slice.len() < 4 {
+            println!("No type info present!");
             return Err(UnexpectedEndOfSlice(UnexpectedEndOfSliceError {
                 layer: error::Layer::VerboseTypeInfo,
                 minimum_size: 4,
@@ -106,25 +115,154 @@ impl<'a> VerboseValue<'a> {
             };
             Ok((Bool(BoolValue { name, value }), slicer.rest()))
         } else if 0 != type_info[0] & SIGNED_FLAG_0 {
-            // verify no conflicting information is present
-            // TODO implement
-            Err(Unsupported)
+            // todo: handle arrays (currently set as contradicting)
+            const CONTRADICTING_MASK_0: u8 = 0b1101_0000;
+            const CONTRADICTING_MASK_1: u8 = 0b0110_0111;
+
+            // check that no contradicting type info is presebt
+            if (0 != type_info[0] & CONTRADICTING_MASK_0)
+                || (0 != type_info[1] & CONTRADICTING_MASK_1)
+            {
+                return Err(InvalidTypeInfo(type_info));
+            }
+
+            let type_len = type_info[0] & 0b1111;
+            match type_len {
+                1 | 2 | 3 | 4 | 5 => {}
+                _ => return Err(InvalidTypeInfo(type_info)),
+            }
+
+            // check for varinfo
+            let name_and_unit = if 0 != type_info[1] & VARINFO_FLAG_1 {
+                Some(slicer.read_var_name_and_unit(is_big_endian)?)
+            } else {
+                None
+            };
+
+            let read_i32_scaling =
+                |slicer: &mut FieldSlicer| -> Result<Option<Scaling<i32>>, VerboseDecodeError> {
+                    if 0 != type_info[1] & FIXED_POINT_FLAG_1 {
+                        Ok(Some(Scaling {
+                            quantization: slicer.read_f32(is_big_endian)?,
+                            offset: slicer.read_i32(is_big_endian)?,
+                        }))
+                    } else {
+                        Ok(None)
+                    }
+                };
+
+            let read_i64_scaling =
+                |slicer: &mut FieldSlicer| -> Result<Option<Scaling<i64>>, VerboseDecodeError> {
+                    if 0 != type_info[1] & FIXED_POINT_FLAG_1 {
+                        Ok(Some(Scaling {
+                            quantization: slicer.read_f32(is_big_endian)?,
+                            offset: slicer.read_i64(is_big_endian)?,
+                        }))
+                    } else {
+                        Ok(None)
+                    }
+                };
+
+            let read_i128_scaling =
+                |slicer: &mut FieldSlicer| -> Result<Option<Scaling<i128>>, VerboseDecodeError> {
+                    if 0 != type_info[1] & FIXED_POINT_FLAG_1 {
+                        Ok(Some(Scaling {
+                            quantization: slicer.read_f32(is_big_endian)?,
+                            offset: slicer.read_i128(is_big_endian)?,
+                        }))
+                    } else {
+                        Ok(None)
+                    }
+                };
+
+            match type_len {
+                1 => Ok((
+                    I8(I8Value {
+                        name: name_and_unit.map(|v| v.0),
+                        unit: name_and_unit.map(|v| v.1),
+                        scaling: read_i32_scaling(&mut slicer)?,
+                        value: slicer.read_i8()?,
+                    }),
+                    slicer.rest(),
+                )),
+                2 => Ok((
+                    I16(I16Value {
+                        name: name_and_unit.map(|v| v.0),
+                        unit: name_and_unit.map(|v| v.1),
+                        scaling: read_i32_scaling(&mut slicer)?,
+                        value: slicer.read_i16(is_big_endian)?,
+                    }),
+                    slicer.rest(),
+                )),
+                3 => Ok((
+                    I32(I32Value {
+                        name: name_and_unit.map(|v| v.0),
+                        unit: name_and_unit.map(|v| v.1),
+                        scaling: read_i32_scaling(&mut slicer)?,
+                        value: slicer.read_i32(is_big_endian)?,
+                    }),
+                    slicer.rest(),
+                )),
+                4 => Ok((
+                    I64(I64Value {
+                        name: name_and_unit.map(|v| v.0),
+                        unit: name_and_unit.map(|v| v.1),
+                        scaling: read_i64_scaling(&mut slicer)?,
+                        value: slicer.read_i64(is_big_endian)?,
+                    }),
+                    slicer.rest(),
+                )),
+                5 => Ok((
+                    I128(I128Value {
+                        name: name_and_unit.map(|v| v.0),
+                        unit: name_and_unit.map(|v| v.1),
+                        scaling: read_i128_scaling(&mut slicer)?,
+                        value: slicer.read_i128(is_big_endian)?,
+                    }),
+                    slicer.rest(),
+                )),
+                _ => unreachable!(),
+            }
         } else if 0 != type_info[0] & UNSIGNED_FLAG_0 {
             // verify no conflicting information is present
             // TODO implement
-            Err(Unsupported)
+            Err(Unsupported(type_info[0], type_info[1]))
         } else if 0 != type_info[0] & FLOAT_FLAG_0 {
             // verify no conflicting information is present
             // TODO implement
-            Err(Unsupported)
+            Err(Unsupported(type_info[0], type_info[1]))
         } else if 0 != type_info[1] & ARRAY_FLAG_1 {
             // verify no conflicting information is present
             // TODO implement
-            Err(Unsupported)
+            Err(Unsupported(type_info[0], type_info[1]))
         } else if 0 != type_info[1] & STRING_FLAG_1 {
+            let len = usize::from(slicer.read_u16(is_big_endian)?);
+
+            let name = if 0 != type_info[1] & VARINFO_FLAG_1 {
+                Some(slicer.read_var_name(is_big_endian)?)
+            } else {
+                None
+            };
+            let parse: Result<&str, str::Utf8Error> = match slicer.read_raw(len) {
+                Ok(valid_parse) => {
+                    if len > 0 {
+                        str::from_utf8(&valid_parse[..valid_parse.len() - 1])
+                    } else {
+                        Ok("")
+                    }
+                }
+                Err(_) => return Err(Unsupported(type_info[0], type_info[1])),
+            };
+
+            match parse {
+                Ok(value) => Ok((Str(StringValue { name, value }), slicer.rest())),
+                Err(_) => Err(Unsupported(type_info[0], type_info[1])),
+            }
+
+            // println!("Result is {str_to_prt}");
             // verify no conflicting information is present
             // TODO implement
-            Err(Unsupported)
+            //Err(Unsupported(type_info[0], type_info[1]))
         } else if 0 != type_info[1] & RAW_FLAG_1 {
             // verify no conflicting information is present+
             const CONTRADICTING_MASK_0: u8 = 0b1111_0000;
@@ -158,11 +296,11 @@ impl<'a> VerboseValue<'a> {
         } else if 0 != type_info[1] & TRACE_INFO_FLAG_1 {
             // verify no conflicting information is present
             // TODO implement
-            Err(Unsupported)
+            Err(Unsupported(type_info[0], type_info[1]))
         } else if 0 != type_info[1] & STRUCT_FLAG_1 {
             // verify no conflicting information is present
             // TODO implement
-            Err(Unsupported)
+            Err(Unsupported(type_info[0], type_info[1]))
         } else {
             // nothing matches type info uninterpretable
             Err(InvalidTypeInfo(type_info))
